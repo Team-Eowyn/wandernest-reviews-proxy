c50f58aefeb98ac520e872823efb01dfd6e97cd9
gen0pp <gen0@protonmail.com>
2020-05-15 14:34:30 -0700

    Added EC2 links to proxy

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -8,6 +8,23 @@ app.use(express.static(path.join(__dirname, './public')));
 // app.use('/:id', express.static(path.join(__dirname, './public')))
 
 // PHOTOCAROUSEL
+app.use('/api/hotels', createProxyMiddleware({target: 'http://ec2-18-220-158-65.us-east-2.compute.amazonaws.com:3001', changeOrigin: true}));
+app.use('/api/hotels/:id', createProxyMiddleware({target: 'http://ec2-18-220-158-65.us-east-2.compute.amazonaws.com:3001', changeOrigin: true}));
+
+// BOOKINGS
+app.use('/api/bookings/:id', createProxyMiddleware({target: 'http://localhost:3002', changeOrigin: true}));
+app.use('/api/bookings', createProxyMiddleware({target: 'http://localhost:3002', changeOrigin: true}));
+
+// ABOUT
+app.use('/about/:id', createProxyMiddleware({target: 'http://localhost:3003', changeOrigin: true}));
+
+// REVIEWS
+app.use('/roomtips', createProxyMiddleware({target: 'http://ec2-54-153-71-183.us-west-1.compute.amazonaws.com:3004', changeOrigin: true}));
+app.use('/qas', createProxyMiddleware({target: 'http://ec2-54-153-71-183.us-west-1.compute.amazonaws.com:3004', changeOrigin: true}));
+app.use('/reviews', createProxyMiddleware({target: 'http://ec2-54-153-71-183.us-west-1.compute.amazonaws.com:3004', changeOrigin: true}));
+
+/*
+// PHOTOCAROUSEL
 app.use('/api/hotels', createProxyMiddleware({target: 'http://localhost:3001', changeOrigin: true}));
 app.use('/api/hotels/:id', createProxyMiddleware({target: 'http://localhost:3001', changeOrigin: true}));
 
@@ -22,6 +39,7 @@ app.use('/about/:id', createProxyMiddleware({target: 'http://localhost:3003', ch
 app.use('/roomtips', createProxyMiddleware({target: 'http://localhost:3004', changeOrigin: true}));
 app.use('/qas', createProxyMiddleware({target: 'http://localhost:3004', changeOrigin: true}));
 app.use('/reviews', createProxyMiddleware({target: 'http://localhost:3004', changeOrigin: true}));
+*/
 
 const port = 3000;
 
